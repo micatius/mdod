63b0eca911d4375537458fc7acd7f5e09b413abe
micatius <dom.mikac1@gmail.com>
2023-07-30 00:26:53 +0200

    added working basic combat and message log

--- tile_types.py
+++ tile_types.py
@@ -38,12 +38,12 @@ SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 floor = new_tile(
     walkable=True,
     transparent=True,
-    dark=(ord("."), (70, 70, 70), (0, 0, 0)),
-    light=(ord("."), (150, 155, 150), (0, 0, 0)),
+    dark=(ord("."), (100, 100, 100), (0, 0, 0)),
+    light=(ord("."), (200, 200, 200), (0, 0, 0)),
 )
 wall = new_tile(
     walkable=False,
     transparent=False,
-    dark=(ord("#"), (70, 70, 70), (0, 0, 0)),
-    light=(ord("#"), (150, 155, 150), (0, 0, 0)),
+    dark=(ord("#"), (100, 100, 100), (0, 0, 0)),
+    light=(ord("#"), (200, 200, 200), (0, 0, 0)),
 )
